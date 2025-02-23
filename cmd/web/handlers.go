3d9352c4b859e63aa777501301592b276dba80b3
shubham88fru <shubham88fru@yahoo.com>
2025-02-22 17:48:17 -0700

    layout complete. onwards to design patterns

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
 	app.render(w, "home.page.gohtml", nil)
 }	
+
+func (app *application) ShowPage(w http.ResponseWriter, r *http.Request) {
+	page := chi.URLParam(r, "page")
+	app.render(w, page+".page.gohtml", nil)
+}
