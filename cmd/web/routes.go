070cd3635384e7425c7d35f87b347e7d30ac36d6
shubham88fru <shubham88fru@yahoo.com>
2025-03-02 18:49:47 -0700

    implement abstract factory pattern

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,6 +20,8 @@ func (app *application) routes() http.Handler {
 	mux.Get("/test-patterns", app.TestPatterns)
 	mux.Get("/api/dog-from-factory", app.CreateDogFromFactory)
 	mux.Get("/api/cat-from-factory", app.CreateCatFromFactory)
+	mux.Get("/api/dog-from-abstract-factory", app.CreateDogFromAbstractFactory)
+	mux.Get("/api/cat-from-abstract-factory", app.CreateCatFromAbstractFactory)
 
 	mux.Get("/", app.ShowHome)
 	mux.Get("/{page}", app.ShowPage)
