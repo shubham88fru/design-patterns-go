3593edcef924750bd561fb999aeae9c4e8256221
shubham88fru <shubham88fru@yahoo.com>
2025-03-02 19:26:06 -0700

    connect maria db

---
+++ cmd/web/db.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	maxOpenDbConn = 25
+	maxIdleDbConn = 25
+	maxDbLifetime = 5 * time.Minute
+)
+
+func initMySqlDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenDbConn)
+	db.SetMaxIdleConns(maxIdleDbConn)
+	db.SetConnMaxLifetime(maxDbLifetime)
+
+	return db, nil
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"html/template"
@@ -13,12 +14,13 @@ const port = ":4000"
 
 type application struct {
 	templateMap map[string]*template.Template
-	config appConfig
-
+	config      appConfig
 }
 
 type appConfig struct {
 	useCache bool
+	dsn      string
+	DB       *sql.DB
 }
 
 func main() {
@@ -27,19 +29,27 @@ func main() {
 	}
 
 	flag.BoolVar(&app.config.useCache, "cache", false, "Use a template cache")
+	flag.StringVar(&app.config.dsn, "dsn", "mariadb:myverysecretpassword@tcp(localhost:3306)/breeders?parseTime=true&tls=false&collation=utf8_unicode_ci&timeout=5s", "DSN")
 	flag.Parse()
 
-	srv := &http.Server{
-		Addr: port,
-		Handler: app.routes(),
-		IdleTimeout: 30*time.Second,
-		ReadTimeout: 5*time.Second,
-		ReadHeaderTimeout: 5*time.Second,
-		WriteTimeout: 10*time.Second,
+	//connect to db
+	db, err := initMySqlDB(app.config.dsn)
+	if err != nil {
+		log.Panic(err)
 	}
-	
+	app.config.DB = db
+
+	srv := &http.Server{
+		Addr:              port,
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       5 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      10 * time.Second,
+	}
+
 	fmt.Println("Starting server on port", port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	if err != nil {
 		log.Panic(err)
