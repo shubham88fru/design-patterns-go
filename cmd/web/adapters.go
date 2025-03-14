d558a4c74ede7a4f088bdf36a3c340654701eeac
shubham88fru <shubham88fru@yahoo.com>
2025-03-14 14:56:37 -0700

    add tests

--- cmd/web/adapters.go
+++ cmd/web/adapters.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"io"
 	"net/http"
 
@@ -42,4 +43,33 @@ func (jb *JSONBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
 	}
 
 	return breeds, nil
+}
+
+type XMLBackend struct{}
+
+func (xb *XMLBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+	resp, err := http.Get("http://localhost:8081/api/cat-breeds/all/xml")
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	type catBreeds struct {
+		XMLName struct{}           `xml:"catBreeds"`
+		Breeds  []*models.CatBreed `xml:"catBreed"`
+	}
+
+	var breeds catBreeds
+	err = xml.Unmarshal(body, &breeds)
+	if err != nil {
+		return nil, err
+	}
+
+	return breeds.Breeds, nil
 }
--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -5,15 +5,33 @@ import (
 	"testing"
 
 	"github.com/shubham88fru/degign-patterns-go/configuration"
+	"github.com/shubham88fru/degign-patterns-go/models"
 )
 
 var testApp application
 
 func TestMain(m *testing.M) {
+	testBackend := &TestBackend{}
+	testAdapter := &RemoteService{Remote: testBackend}
 
 	testApp = application{
-		App: configuration.New(nil),
+		App:        configuration.New(nil),
+		CatService: testAdapter,
 	}
 
 	os.Exit(m.Run())
+}
+
+type TestBackend struct{}
+
+func (tb *TestBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+	breeds := []*models.CatBreed{
+		&models.CatBreed{
+			ID:      1,
+			Breed:   "Tomcat",
+			Details: "Some details",
+		},
+	}
+
+	return breeds, nil
 }
